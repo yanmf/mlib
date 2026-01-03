d1391ae3c448bcb12a8b900f909d6d9a8554b569
yanmf <sxycymf@126.com>
2026-01-03 21:45:25 +0800

    提交无用代码

--- zdef/defs.go
+++
@@ -1,7 +0,0 @@
-package zdef
-
-type SessionConf struct {
-	ServiceName string `json:"ServiceName,omitempty"` // game gate ...
-	ID          int64  `json:"ID,omitempty"`
-	Host        string `json:"Host,omitempty"`
-}
